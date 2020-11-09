136df6c7ba5780d9db5abdc3f5cb50d80b4de2a5
atidele <alejandro.tidele@mercadolibre.com>
2020-11-09 13:45:42 -0300

    Starting with Firebase 🔥

--- app/src/main/java/src/Activities/ui/history_form/HistoryFormFragment.java
+++ app/src/main/java/src/Activities/ui/history_form/HistoryFormFragment.java
@@ -1,18 +1,10 @@
 package src.Activities.ui.history_form;
 
-import android.app.AlertDialog;
 import android.content.Context;
-import android.content.Intent;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.drawable.BitmapDrawable;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -22,16 +14,12 @@ import androidx.lifecycle.ViewModelProvider;
 
 import com.example.tp_cuatrimestral.R;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.Objects;
-
-import components.WithImageFragment.WithImageFragment;
 import src.Activities.SystemActivity;
 import src.Builders.HistoryFormBuilder;
 import src.Models.History;
+import src.Services.Entities.AlertService;
 
-public class HistoryFormFragment extends WithImageFragment {
+public class HistoryFormFragment extends Fragment {
 
     private HistoryFormViewModel historyFormViewModel;
     private static History history;
@@ -48,16 +36,12 @@ public class HistoryFormFragment extends WithImageFragment {
                              @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_history_form, container, false);
 
-        ((TextView) root.findViewById(R.id.button_image))
-                .setOnClickListener(view -> openGallery(1));
-
-        ((TextView) root.findViewById(R.id.link_view_loaded_image))
-                .setOnClickListener(view -> showImage());
-
         ((TextView) root.findViewById(R.id.button_save))
-                .setOnClickListener(view -> SystemActivity.performClick(R.id.nav_manage_history));
+                .setOnClickListener(view -> {
+                    AlertService.save();
+                });
 
-        ((TextView) root.findViewById(R.id.link_cancel))
+        ((TextView) root.findViewById(R.id.link_view_history))
                 .setOnClickListener(view -> SystemActivity.performClick(R.id.nav_manage_history));
 
         return root;
@@ -69,15 +53,7 @@ public class HistoryFormFragment extends WithImageFragment {
         historyFormViewModel = new ViewModelProvider(this).get(HistoryFormViewModel.class);
     }
 
-    private void beforeSave() {
-        if (this.getImage() == null) {
-            // ToDo: Ask if is ok continue without an image.
-        }
-    }
-
     private void onSave() {
-        this.beforeSave();
-
         History history = new HistoryFormBuilder()
                 .build();
     }
--- app/src/main/java/src/Services/Entities/AlertService.java
+++ app/src/main/java/src/Services/Entities/AlertService.java
@@ -1,18 +1,55 @@
 package src.Services.Entities;
 
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 import src.Models.Alert;
 
 public abstract class AlertService {
-    private static List<Alert> alertList = Arrays.asList(
-            new Alert(),
-            new Alert(),
-            new Alert()
-    );
+    private static List<Alert> alertList;
 
     public static List<Alert> getAlertList() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("alertas").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                alertList = new ArrayList<>();
+                for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
+                    System.out.println(document.getData());
+                    alertList.add(new Alert());
+                }
+            } else {
+                System.out.println("ERROR!");
+            }
+        });
         return alertList;
+    }
+
+    public static void save() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("name", "Ale");
+        user.put("contact", "something");
+        user.put("observations", "Lorem ipsum");
+
+        db.collection("alertas")
+                .add(user)
+                .addOnSuccessListener(documentReference -> System.out.println(documentReference.getId()))
+                .addOnFailureListener(Throwable::printStackTrace);
     }
 }
