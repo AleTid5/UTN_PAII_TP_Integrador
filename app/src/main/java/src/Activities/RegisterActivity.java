1ba9c0a2e56ccc96288e9e1ed5877452fbe93409
Dangelo Martin <martinalexisdangelo@gmail.com>
2020-11-09 20:12:21 -0300

    wip

--- app/src/main/java/src/Activities/RegisterActivity.java
+++ app/src/main/java/src/Activities/RegisterActivity.java
@@ -3,6 +3,7 @@ package src.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,5 +21,11 @@ public class RegisterActivity extends AppCompatActivity {
         startActivity(new Intent(this, LoginActivity.class));
     }
 
-    public void onRegister(View view) {}
+    public void onRegister(View view) {
+        try {
+            String name=((TextView)view.findViewById(R.id.input_name)).getText().toString();
+        }catch (Exception e) {
+
+        }
+    }
 }
