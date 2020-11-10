deeb3c1832d775817cf294a3f31121979334ec26
Dangelo Martin <martinalexisdangelo@gmail.com>
2020-11-10 20:00:19 -0300

    Se agrega validacion de datos Se agrega EmailValidator

--- app/src/main/java/src/Activities/RegisterActivity.java
+++ app/src/main/java/src/Activities/RegisterActivity.java
@@ -8,6 +8,11 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tp_cuatrimestral.R;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Objects;
+
+import src.Validators.EmailValidator;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -23,9 +28,24 @@ public class RegisterActivity extends AppCompatActivity {
 
     public void onRegister(View view) {
         try {
-            String name=((TextView)view.findViewById(R.id.input_name)).getText().toString();
-        }catch (Exception e) {
+            String name=((TextView)view.findViewById(R.id.input_name)).getText().toString().trim();
+            if (name.length()<5){
+                throw new Exception("El nombre no puede ser menor a 5 caracteres");
+            }
+            String dni=((TextView)view.findViewById(R.id.input_dni)).getText().toString().trim();
+            if (dni.length()<7) {
+                throw new Exception("Ingrese un dni valido");
+            }
+            String username=((TextView)view.findViewById(R.id.input_username)).getText().toString().trim();
+            if (username.length()<5) {
+                throw new Exception("El Nombre de Usuario no puede ser menor a 5 caracteres");
+            }
+            String email=((TextView)view.findViewById(R.id.input_email)).getText().toString().trim();
+            EmailValidator.validateEmail(email);
 
+        }catch (Exception e) {
+            Snackbar.make(view, Objects.requireNonNull(e.getMessage()), Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show();
         }
     }
 }
