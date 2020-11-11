838216c07f15f8a4f9e2457d66ee05cf9b910ae8
DESKTOP-7LVTRMR\corin <emaxi11737@gmail.com>
2020-11-10 21:35:07 -0300

    Se agrega el servicio de recuperacion de contrasenia

--- app/src/main/java/src/Activities/RecoveryPasswordActivity.java
+++ app/src/main/java/src/Activities/RecoveryPasswordActivity.java
@@ -3,10 +3,15 @@ package src.Activities;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tp_cuatrimestral.R;
+
+import components.Snackbar.CustomSnackbar;
+import src.Services.Entities.PasswordRecoveryService;
+import src.Validators.NumberValidator;
 
 public class RecoveryPasswordActivity extends AppCompatActivity {
 
@@ -20,5 +25,14 @@ public class RecoveryPasswordActivity extends AppCompatActivity {
         startActivity(new Intent(this, LoginActivity.class));
     }
 
-    public void onRecoveryPassword(View view) {}
+    public void onRecoveryPassword(View view) {
+        try{
+            String textDNI=((TextView)this.findViewById(R.id.input_dni)).getText().toString().trim();
+            Integer DNI=NumberValidator.validateDNI(textDNI);
+            PasswordRecoveryService.recoverPassword(DNI);
+
+        } catch(Exception ex){
+            new CustomSnackbar(view,ex.getMessage()).danger();
+        }
+    }
 }
