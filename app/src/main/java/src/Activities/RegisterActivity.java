f0c8709345eba45f94e2131c3359fc9d2ed243fc
Dangelo Martin <martinalexisdangelo@gmail.com>
2020-11-10 22:17:04 -0300

    Se agraga validaciones al registro

--- app/src/main/java/src/Activities/RegisterActivity.java
+++ app/src/main/java/src/Activities/RegisterActivity.java
@@ -12,7 +12,10 @@ import com.google.android.material.snackbar.Snackbar;
 
 import java.util.Objects;
 
+import components.Snackbar.CustomSnackbar;
+import src.Validators.DateValidator;
 import src.Validators.EmailValidator;
+import src.Validators.PasswordValidator;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -28,24 +31,34 @@ public class RegisterActivity extends AppCompatActivity {
 
     public void onRegister(View view) {
         try {
-            String name=((TextView)view.findViewById(R.id.input_name)).getText().toString().trim();
+            String name=((TextView)this.findViewById(R.id.input_name)).getText().toString().trim();
             if (name.length()<5){
                 throw new Exception("El nombre no puede ser menor a 5 caracteres");
             }
-            String dni=((TextView)view.findViewById(R.id.input_dni)).getText().toString().trim();
+            String dni=((TextView)this.findViewById(R.id.input_dni)).getText().toString().trim();
             if (dni.length()<7) {
                 throw new Exception("Ingrese un dni valido");
             }
-            String username=((TextView)view.findViewById(R.id.input_username)).getText().toString().trim();
+            String date=((TextView)this.findViewById(R.id.input_born_date)).getText().toString().trim();
+            DateValidator.validateDateNotGreaterThanToday(date);
+
+            String username=((TextView)this.findViewById(R.id.input_username)).getText().toString().trim();
             if (username.length()<5) {
                 throw new Exception("El Nombre de Usuario no puede ser menor a 5 caracteres");
             }
-            String email=((TextView)view.findViewById(R.id.input_email)).getText().toString().trim();
+            String email=((TextView)this.findViewById(R.id.input_email)).getText().toString().trim();
             EmailValidator.validateEmail(email);
 
+            String password=((TextView)this.findViewById(R.id.input_password)).getText().toString().trim();
+            PasswordValidator.validatePassword(password);
+
+            String passwordRepeat=((TextView)this.findViewById(R.id.input_password_repeat)).getText().toString().trim();
+            if (!password.equals(passwordRepeat)){
+                throw new Exception("Las Contraseñas no coinciden");
+            }
+
         }catch (Exception e) {
-            Snackbar.make(view, Objects.requireNonNull(e.getMessage()), Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show();
+            new CustomSnackbar(view,Objects.requireNonNull(e.getMessage())).danger();
         }
     }
 }
