36da991ef133bd1779f395f1b8ad96bb0d6b9710
LucianoDiaz <luchodiaz1995@gmail.com>
2020-11-13 18:22:33 -0300

    agrego la primer bara de progeso en el dash sobre historiales

--- app/src/main/java/src/Activities/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/src/Activities/ui/dashboard/DashboardFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,11 +16,9 @@ import com.example.tp_cuatrimestral.R;
 
 public class DashboardFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
+        DashboardViewModel dashboardViewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
         View root = inflater.inflate(R.layout.fragment_main_layout, container, false);
 
         ((TextView) root.findViewById(R.id.main_title)).setText("Dashboard");
@@ -27,6 +26,20 @@ public class DashboardFragment extends Fragment {
         FrameLayout mainContent = root.findViewById(R.id.main_content);
         View content = getLayoutInflater().inflate(R.layout.fragment_dashboard, mainContent, false);
         mainContent.addView(content);
+
+        dashboardViewModel.getLiveTotal().observe(getViewLifecycleOwner(), total -> {
+            if (total == null) return;
+
+            ProgressBar progressBar = root.findViewById(R.id.progressBar_history_reports);
+            progressBar.setIndeterminate(false);
+            progressBar.setProgress(dashboardViewModel.getLiveHistoryReportsTotal());
+            progressBar.setMax(total);
+            ((TextView) root.findViewById(R.id.text_history_reports)).setText(String.format(
+                    "Cantidad de historiales (%s)",
+                    dashboardViewModel.getLiveHistoryReportsTotal()
+            ));
+
+        });
 
         return root;
     }
