d1dde043441386b106b53e33c7a3dd30f1260efb
LucianoDiaz <luchodiaz1995@gmail.com>
2020-11-13 18:18:19 -0300

    creo las variables de la clase

--- app/src/main/java/src/Activities/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/src/Activities/ui/dashboard/DashboardViewModel.java
@@ -1,6 +1,12 @@
 package src.Activities.ui.dashboard;
 
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class DashboardViewModel extends ViewModel {
+    private static Integer liveHistoryReportsTotal;
+    private static Integer liveAlertsTotal;
+    private static Integer liveBlockedUsersTotal;
+    private static MutableLiveData<Integer> liveTotalCount = new MutableLiveData<>();
+
 }
