d440e6fa8d8e314c74efe99689419bf5816afa21
LucianoDiaz <luchodiaz1995@gmail.com>
2020-11-13 18:27:39 -0300

    agrego los metodos de la clase

--- app/src/main/java/src/Activities/ui/dashboard/DashboardViewModel.java
+++ app/src/main/java/src/Activities/ui/dashboard/DashboardViewModel.java
@@ -16,4 +16,38 @@ public class DashboardViewModel extends ViewModel {
         liveTotalCount.setValue(null);
         DashboardService.initializeDashboard();
     }
+
+    public LiveData<Integer> getLiveTotal() {
+        return liveTotalCount;
+    }
+
+    public Integer getLiveHistoryReportsTotal() {
+        return liveHistoryReportsTotal;
+    }
+
+    public Integer getLiveAlertsTotal() {
+        return liveAlertsTotal;
+    }
+
+    public Integer getLiveBlockedUsersTotal() {
+        return liveBlockedUsersTotal;
+    }
+
+    public static void setLiveHistoryReportsTotal(Integer count) {
+        liveHistoryReportsTotal = count;
+    }
+
+    public static void setLiveAlertsTotal(Integer count) {
+        liveAlertsTotal = count;
+    }
+
+    public static void setLiveBlockedUsersTotal(Integer count) {
+        liveBlockedUsersTotal = count;
+    }
+
+    public static void updateTotal(Integer total) {
+        if (total > 0) {
+            liveTotalCount.postValue(total);
+        }
+    }
 }
